82716d1a6ca455b438dea93141e1643817b86e96
Juan Sanchez <juan.sanchez@automat-it.com>
2024-04-22 09:21:24 +0200

    add problems

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = {
     roll: function () {
+        letsAddSomeUnusedDuplicatedVariable = 'test'
+        letsAddSomeUnusedDuplicatedVariable = 'test'
+        letsAddSomeUnusedDuplicatedVariable = 'test'
+        letsAddSomeUnusedDuplicatedVariable = 'test'
+        letsAddSomeUnusedDuplicatedVariable = 'test'
+        letsAddSomeUnusedDuplicatedVariable = 'test'
         return Math.floor(Math.random()*6)+1;
     }
 };

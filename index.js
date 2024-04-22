d5604926ab70e5f001410a25fe96ae73d6de1ecc
Juan Sanchez <juan.sanchez@automat-it.com>
2024-04-22 10:59:28 +0200

    Test brach protection

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
     roll: function () {
+        letsAddSomeUnusedDuplicatedVariable = '1'
+        letsAddSomeUnusedDuplicatedVariable = '1'
+        compareValue = 1
+        console.log(letsAddSomeUnusedDuplicatedVariable == compareValue)
         return Math.floor(Math.random()*6)+1;
     }
 };
